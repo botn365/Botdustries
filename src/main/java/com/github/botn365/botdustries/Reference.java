fd08b0dc5b48419c0acb28bf8f6f984776dd7d4f
korneel vandamme <Krampus.sack.never@gmail.com>
2021-02-18 22:22:02 +0100

    bump version

--- src/main/java/com/github/botn365/botdustries/Reference.java
+++ src/main/java/com/github/botn365/botdustries/Reference.java
@@ -5,7 +5,7 @@ public final class Reference {
     //The location of this file must be defined in gradle.properties under project_reference
     public static final String MODID = "botdustries";
     public static final String NAME = "Botdustries";
-    public static final String VERSION = "1.0.1.0";
+    public static final String VERSION = "1.1.0.0";
 
     private Reference() {}
 }
